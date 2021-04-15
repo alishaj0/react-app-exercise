33b2a523c57c2eb82880b9bef7005d669248d93b
Alisha Bolin <alishabolin@gmail.com>
2021-04-15 10:11:31 -0500

    Added another way to do bonus problem and updated my variables in original bonus answer.

--- src/bonus/Bonus.js
+++ src/bonus/Bonus.js
@@ -1,29 +1,52 @@
-import { useState, useEffect } from 'react';
+// BONUS
+
+// { useState } is a function or a hook used to import this property from 'react' feature
+import { useState } from 'react';
+
 
 const Bonus = () => {
     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     const [letter, setNextLetter] = useState(0);
 
-    const handleAssigneeOnClick = () => {
+    const nextLetter = () => {
 
-        setNextLetter(prev => {
-            if (prev === letters.length - 1) {
+        setNextLetter(letter => {
+            if (letter === letters.length - 1) {
                 return 0;
             } else {
-                return prev + 1;
+                return letter + 1;
             }
         });}
 
         return(
             <div>
                 <h1>{letters[letter]}</h1>
-                <button onClick={handleAssigneeOnClick}>NEXT LETTER</button>
+                <button onClick={nextLetter}>NEXT LETTER</button>
 
             </div>
         );
-
-
 }
-    
+
+
+// ALSO --- With Ternary
+
+   
+// const Bonus = () => {
+//     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+//     const [letter, setLetter] = useState(0);
+
+//     return(
+//         <div>
+//         {/* Using [letters array, plugging in an index, to give certain letter from 'letters' array] */}
+//             <h1>{letters[letter]}</h1>
+//         {/* {letters[0] = A */}
+//         {/* onClick= will run anonymous arrow function, using letter[0], is less than  letters.length - 1, it will setLetter(letter + 1).... if letters is not less than  (letters.length - 1) , it will setLetter back to 0(*/}
+//             <button onClick={() => letter < (letters.length - 1) ? setLetter(letter + 1) : setLetter(0)}>NEXT LETTER</button>
+//         </div>
+//     );
+
+// }
+
 
 export default Bonus;
